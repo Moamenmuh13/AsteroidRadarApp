9f6fbfa6ca9fa4006e3928be7b2300833e412636
Mu'men Muhammad <61587687+Moamenmuh13@users.noreply.github.com>
2022-08-12 01:02:03 +0200

    init room

--- app/src/main/java/com/udacity/asteroidradar/network/AsteroidServices.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/AsteroidServices.kt
@@ -7,6 +7,7 @@ import com.udacity.asteroidradar.model.Asteroid
 import com.udacity.asteroidradar.model.PictureOfDay
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -27,10 +28,9 @@ object APIServices {
         .add(KotlinJsonAdapterFactory())
         .build()
 
-
     private val retrofit = Retrofit.Builder()
         .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addConverterFactory(ScalarsConverterFactory.create())
         .build()
 
     val asteroidServices: AsteroidServices by lazy {
